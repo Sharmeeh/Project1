77311e92d7f1e7851c9883ba4e4af1fa1629689a
Sharmila Nagarajan <120065235+Sharmeeh@users.noreply.github.com>
2025-04-16 11:20:33 +1000

    Add DockerfileAdd updated app.py

--- app.py
+++ app.py
@@ -1,16 +1,9 @@
-from flask import Flask,render_template
-import socket
-
+from flask 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    try:
-        host_name = socket.gethostname()
-        host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip)
-    except:
-        return render_template('error.html')
+@app.route('/')
+def home():
+    return "Hello from Jenkins Pipeline Docker app!"
 
 
 if __name__ == "__main__":
